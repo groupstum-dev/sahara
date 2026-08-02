0111930c2d1dcc8d23ba68940ec7299b87e745bb
groupstum-dev <groupstum@gmail.com>
2026-08-02 20:13:33 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <main>
+      <h1>Sahara Crowdfunding Platform</h1>
+      <p>Empowering African innovators through global support.</p>
+    </main>
+  );
+}
