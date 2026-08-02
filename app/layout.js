d644345ec1ddd8846090ffc3b2d8e56f952e4d4b
groupstum-dev <groupstum@gmail.com>
2026-08-02 21:24:59 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,31 @@
 import "./globals.css";
 
+
 export const metadata = {
-  title: "Sahara | African Innovation Crowdfunding",
-  description:
-    "A reward-based crowdfunding platform connecting African innovators with global supporters."
+
+title:"Sahara",
+
+description:
+"Reward crowdfunding platform for African innovators"
+
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+
+
+export default function RootLayout({children}){
+
+return (
+
+<html lang="en">
+
+<body>
+
+{children}
+
+</body>
+
+</html>
+
+);
+
 }
